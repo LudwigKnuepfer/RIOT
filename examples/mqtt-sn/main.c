aef944ac991cfa312a8a6f19d55f5d32a67ee7d1
Ludwig Knüpfer <ludwig.knuepfer@fu-berlin.de>
2015-10-22 22:48:23 +0200

    examples/mqtt-sn: initial import

---
+++ examples/mqtt-sn/main.c
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2015 Cenk Gündoğan
+ * Copyright (C) 2015 Ludwig Knüpfer
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     examples
+ * @{
+ *
+ * @file
+ * @brief       Example application for mqtt-sn
+ *
+ * @author      Cenk Gündoğan <cnkgndgn@gmail.com>
+ * @author      Ludwig Knüpfer <ludwig.knuepfer@fu-berlin.de>
+ *
+ * @}
+ */
+
+#include <stdio.h>
+
+#include "shell.h"
+#include "msg.h"
+
+#include "mqtt_shell.h"
+
+#define MAIN_QUEUE_SIZE     (8)
+static msg_t _main_msg_queue[MAIN_QUEUE_SIZE];
+
+static const shell_command_t shell_commands[] = {
+    {"sub", "subscribe to topic", mqtt_shell_subscribe},
+    {"pub", "publish message to topic", mqtt_shell_publish},
+    {"con", "connect to a server", mqtt_shell_connect},
+    {"dis", "disconnect from server", mqtt_shell_disconnect},
+    {NULL, NULL, NULL}
+};
+
+mqttsn_state_t mqtt_state;
+
+int main(void)
+{
+    puts("RIOT MQTT-SN example application");
+
+    /* we need a message queue for the thread running the shell in order to
+     * receive potentially fast incoming networking packets */
+    msg_init_queue(_main_msg_queue, MAIN_QUEUE_SIZE);
+
+    /* initialize mqtt */
+    mqttsn_connect();
+
+    /* start shell */
+    puts("Starting the shell");
+    char line_buf[SHELL_DEFAULT_BUFSIZE];
+    shell_run(shell_commands, line_buf, SHELL_DEFAULT_BUFSIZE);
+
+    /* not reached */
+    return 0;
+}
---
+++ examples/mqtt-sn/mqtt_shell.h
@@ -0,0 +1,9 @@
+#ifndef MQTT_SHELL_H
+#define MQTT_SHELL_H
+
+void mqtt_shell_subscribe(void);
+void mqtt_shell_publish(void);
+void mqtt_shell_connect(void);
+void mqtt_shell_disconnect(void);
+
+#endif /* MQTT_SHELL_H */
